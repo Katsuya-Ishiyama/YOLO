0c18c524401d0cdee5c3aaf60aebdeeea6f9282b
katsuya-ishiyama <katsuya.ishiyama@enish.com>
2018-09-11 13:03:51 +0900

    fixed Leaky ReLU

--- yolo.py
+++ yolo.py
@@ -6,8 +6,8 @@ from keras.layers.normalization import BatchNormalization
 from keras.models import Model
 
 
-def LeakyReLU(x):
-    return keras.layers.LeakyReLU(x, alpha=0.1)
+def LeakyReLU():
+    return keras.layers.LeakyReLU(alpha=0.1)
 
 
 def build_model(inputs: Input):
@@ -17,7 +17,7 @@ def build_model(inputs: Input):
     x = Conv2D(kernel_size=(7, 7), filters=64, strides=2, padding='same')(inputs)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
-    x = LeakyReLU(x)
+    x = LeakyReLU()(x)
 
     # -----------------------------------------------------
     # Block 2
@@ -25,7 +25,7 @@ def build_model(inputs: Input):
     x = Conv2D(kernal_size=(3, 3), filters=192, strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
-    x = LeakyReLU(x)
+    x = LeakyReLU()(x)
 
     # -----------------------------------------------------
     # Block 3
@@ -39,7 +39,7 @@ def build_model(inputs: Input):
     x = Conv2D(kernel_size=(3, 3), filters=512, strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
-    x = LeakyReLU(x)
+    x = LeakyReLU()(x)
 
     # -----------------------------------------------------
     # Block 4
@@ -64,7 +64,7 @@ def build_model(inputs: Input):
     x = Conv2D(kernel_size=(3, 3), filters=1024, strides=1, padding='same')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
-    x = LeakyReLU(x)
+    x = LeakyReLU()(x)
 
     # -----------------------------------------------------
     # Block 5
@@ -80,7 +80,7 @@ def build_model(inputs: Input):
 
     x = Conv2D(kernel_size=(3, 3), filters=1024, strides=2, padding='same')(x)
     x = BatchNormalization()(x)
-    x = LeakyReLU(x)
+    x = LeakyReLU()(x)
 
     # -----------------------------------------------------
     # Block 6
@@ -89,8 +89,7 @@ def build_model(inputs: Input):
     x = BatchNormalization()(x)
     x = Conv2D(kernel_size=(3, 3), filters=1024, strides=1, padding='same')(x)
     x = BatchNormalization()(x)
-    x = LeakyReLU(x)
-
+    x = LeakyReLU()(x)
 
     x = Dense(units=4096, activation=LeakyReLU)
     x = Dense(units=7*7*30, activation=LeakyReLU)
